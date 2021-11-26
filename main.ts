337232ddeb125f6897c0a50a95ebbc8acf2e796e
Cam-g-911 <93203511+Cam-g-911@users.noreply.github.com>
2021-11-26 14:40:55 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,41 @@
+function jaune () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+}
+function blanc () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+}
+function orange () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+}
+function rouge () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+}
+function vert () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+}
+basic.forever(function () {
+    vert()
+    basic.pause(2000)
+    jaune()
+    basic.pause(700)
+    rouge()
+    basic.pause(2000)
+    if (input.buttonIsPressed(Button.A)) {
+        blanc()
+        basic.pause(200)
+        orange()
+        basic.pause(200)
+    } else {
+    	
+    }
+})
